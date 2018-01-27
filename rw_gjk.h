fe9237f6ed21947b73f96eaed913105c7539442f
Robin Wragg <robin.wragg@me.com>
2018-01-27 21:23:36 +0000

    Switched casings.

--- rw_gjk.h
+++ rw_gjk.h
@@ -8,8 +8,13 @@ Written by Robin Wragg.
 See end of file for license information.
 */
 
-#ifndef RW_GJK_H
-#define RW_GJK_H
+// TODO: check for NaN?
+// TODO: remove occurrences of while (true).
+// TODO: remove asserts and handle those states in a graceful way.
+// TODO: add caching of rotations.
+
+#ifndef RW_GJK_HEADER
+#define RW_GJK_HEADER
 
 #include <vector>
 
@@ -24,10 +29,10 @@ namespace rw_gjk {
 		
 		double length() const;
 		double distance(const v2 &) const;
-		bool isZero() const;
-		v2 normalisedOrZero() const;
-		v2 rightNormalOrZero() const;
-		v2 normalInDirectionOrZero(v2 direction) const;
+		bool is_zero() const;
+		v2 normalised_or_zero() const;
+		v2 right_normal_or_zero() const;
+		v2 normal_in_direction_or_0(v2 direction) const;
 		v2 rotated(double radians) const;
 		
 		bool operator==(const v2 &) const;
@@ -46,24 +51,19 @@ namespace rw_gjk {
 		vector<v2> corners; // TODO: Make private?
 	};
 	
-	struct OverlapInfo {
+	struct Overlap_Info {
 		bool overlapping;
 		v2 amount; // TODO: rename?
 	};
 	
-	bool tryMakeShape(vector<v2> corners, Shape *shapeOut);
+	bool try_make_shape(vector<v2> corners, Shape *shape_out);
 	
-	OverlapInfo getOverlapInfo(Shape *shapeA, Shape *shapeB);
+	Overlap_Info get_overlap_info(Shape *a, Shape *b);
 }
 
 #endif
 
 #ifdef RW_GJK_IMPLEMENTATION
-
-// TODO: check for NaN.
-// TODO: remove occurrences of while (true).
-// TODO: look at better ways to iterate over vectors.
-// TODO: remove asserts and handle those states in a graceful way.
 
 #include <cmath>
 #include <cassert>
@@ -76,7 +76,11 @@ namespace rw_gjk {
 		return a.x*b.x + a.y*b.y;
 	}
 	
-	v2::v2() {}
+	v2::v2() {
+		// These are NANs at the moment to catch uninitialised-variable bugs
+		x = NAN;
+		y = NAN;
+	}
 	
 	v2::v2(double x_, double y_) {
 		x = x_;
@@ -91,12 +95,12 @@ namespace rw_gjk {
 		return hypot(x - rh.x, y - rh.y);
 	}
 	
-	bool v2::isZero() const {
+	bool v2::is_zero() const {
 		return x == 0 && y == 0;
 	}
 	
-	v2 v2::normalisedOrZero() const {
-		if (isZero()) {
+	v2 v2::normalised_or_zero() const {
+		if (is_zero()) {
 			return v2(0, 0);
 		}
 		
@@ -105,21 +109,21 @@ namespace rw_gjk {
 		return *this / l;
 	}
 	
-	v2 v2::rightNormalOrZero() const {
-		return v2(y, -x).normalisedOrZero();
+	v2 v2::right_normal_or_zero() const {
+		return v2(y, -x).normalised_or_zero();
 	}
 	
-	v2 v2::normalInDirectionOrZero(v2 direction) const {
-		v2 normal0 = rightNormalOrZero(); // TODO: I don't think this needs to be normalised until we return.
-		double dotResult = dot(normal0, direction);
+	v2 v2::normal_in_direction_or_0(v2 direction) const {
+		v2 normal_a = right_normal_or_zero(); // TODO: I don't think this needs to be normalised until we return.
+		double dotResult = dot(normal_a, direction);
 		
 		if (dotResult > 0) {
-			assert(fabs(dot(normal0, *this)) < TINY_NUMBER);
-			return normal0;
+			assert(fabs(dot(normal_a, *this)) < TINY_NUMBER);
+			return normal_a;
 		} else if (dotResult < 0) {
-			v2 normal1 = normal0 * -1;
-			assert(fabs(dot(normal1, *this)) < TINY_NUMBER);
-			return normal1;
+			v2 normal_b = normal_a * -1;
+			assert(fabs(dot(normal_b, *this)) < TINY_NUMBER);
+			return normal_b;
 		} else {
 			return v2(0, 0);
 		}
@@ -157,9 +161,8 @@ namespace rw_gjk {
 	}
 	
 	v2 origin = v2(0, 0);
-	string errorDescription = "";
 	
-	bool containsDuplicates(vector<v2> vertices) {
+	bool contains_duplicates(vector<v2> vertices) {
 		for (int s0 = 0; s0 < vertices.size(); s0++) {
 			for (int s1 = s0+1; s1 < vertices.size(); s1++) {
 				if (vertices[s0] == vertices[s1]) return true;
@@ -168,13 +171,12 @@ namespace rw_gjk {
 		return false;
 	}
 	
-	bool tryMakeShape(vector<v2> corners, Shape *shapeOut) {
+	bool try_make_shape(vector<v2> corners, Shape *shape_out) {
 		if (corners.size() < 3) {
-			errorDescription = "Less than 3 corners were given to allocShape().";
 			return false;
 		}
 		
-		if (containsDuplicates(corners)) {
+		if (contains_duplicates(corners)) {
 			return false;
 		}
 		
@@ -183,42 +185,42 @@ namespace rw_gjk {
 		// check if the corners form a convex shape
 		{
 			// add the leftmost corner to the shape
-			v2 leftmostCorner = corners.front();
+			v2 leftmost_corner = corners.front();
 			for (auto &corner: corners) {
-				if (corner.x < leftmostCorner.x) leftmostCorner = corner;
+				if (corner.x < leftmost_corner.x) leftmost_corner = corner;
 			}
-			shape.corners.push_back(leftmostCorner);
+			shape.corners.push_back(leftmost_corner);
 			
 			// build a convex shape out of the corners, in any order.
-			v2 searchDir = v2(0, 1);
+			v2 search_dir = v2(0, 1);
 			while (true) {
 				// find the next corner
-				double highestCornerDot = -INFINITY;
-				v2 bestCorner;
+				double highest_corner_dot = -INFINITY;
+				v2 best_corner;
 				for (auto &corner: corners) {
 					if (corner == shape.corners.back()) continue;
 					
-					v2 cornerDifference = corner - shape.corners.back();
-					double cornerDot = dot(searchDir, cornerDifference.normalisedOrZero());
+					v2 corner_difference = corner - shape.corners.back();
+					double corner_dot = dot(search_dir, corner_difference.normalised_or_zero());
 					
-					if (cornerDot >= 0 && cornerDot > highestCornerDot) {
-						highestCornerDot = cornerDot;
-						bestCorner = corner;
+					if (corner_dot >= 0 && corner_dot > highest_corner_dot) {
+						highest_corner_dot = corner_dot;
+						best_corner = corner;
 					}
 				}
 				
 				// if a valid corner was found
-				if (highestCornerDot >= 0) {
+				if (highest_corner_dot >= 0) {
 					// check if it's the same as the first corner
-					if (bestCorner == shape.corners[0]) break; // the shape is complete
+					if (best_corner == shape.corners[0]) break; // the shape is complete
 					else {
 						// add the corner to the shape and update the search direction
-						searchDir = (bestCorner - shape.corners.back()).normalisedOrZero();
-						shape.corners.push_back(bestCorner);
+						search_dir = (best_corner - shape.corners.back()).normalised_or_zero();
+						shape.corners.push_back(best_corner);
 					}
 				} else {
 					// no valid corner was found in the current search direction, so rotate it 90 degrees
-					searchDir = searchDir.rightNormalOrZero();
+					search_dir = search_dir.right_normal_or_zero();
 				}
 			}
 			
@@ -235,37 +237,37 @@ namespace rw_gjk {
 			if (corner.length() > shape.radius) shape.radius = corner.length();
 		}
 		
-		*shapeOut = shape;
-		shapeOut->pos = origin;
-		shapeOut->angle = 0;
+		*shape_out = shape;
+		shape_out->pos = origin;
+		shape_out->angle = 0;
 		
 		return true;
 	}
 	
-	v2 getMinkowskiDiffedEdge(Shape *shape, Shape *otherShape, v2 direction) {
+	v2 get_minkowski_diffed_edge(Shape *shape, Shape *other_shape, v2 direction) {
 		auto getEdgeOfRotatedShape = [](Shape *shape, v2 direction) {
-			v2 bestCrnr;
-			double bestDot = -INFINITY;
+			v2 best_corner;
+			double best_dot = -INFINITY;
 			
 			for (const auto &corner: shape->corners) {
-				v2 rotatedCrnr = corner.rotated(shape->angle);
-				double newDot = dot(rotatedCrnr, direction);
+				v2 rotated_corner = corner.rotated(shape->angle);
+				double new_dot = dot(rotated_corner, direction);
 				
-				if (newDot > bestDot) {
-					bestCrnr = rotatedCrnr;
-					bestDot = newDot;
+				if (new_dot > best_dot) {
+					best_corner = rotated_corner;
+					best_dot = new_dot;
 				}
 			}
 			
-			return bestCrnr;
+			return best_corner;
 		};
 		
-		v2 shapeWorldEdge = shape->pos + getEdgeOfRotatedShape(shape, direction);
-		v2 otherShapeWorldEdge = otherShape->pos + getEdgeOfRotatedShape(otherShape, -direction);
-		return shapeWorldEdge - otherShapeWorldEdge;
+		v2 shape_world_edge = shape->pos + getEdgeOfRotatedShape(shape, direction);
+		v2 other_shape_world_edge = other_shape->pos + getEdgeOfRotatedShape(other_shape, -direction);
+		return shape_world_edge - other_shape_world_edge;
 	}
 	
-	bool originIsBetweenPoints(v2 a, v2 b) {
+	bool origin_is_between_points(v2 a, v2 b) {
 		v2 ao = origin - a;
 		v2 bo = origin - b;
 		v2 ab = b - a;
@@ -273,184 +275,184 @@ namespace rw_gjk {
 		return (dot(ao, ab) > 0) && (dot(bo, ba) > 0);
 	}
 	
-	bool improve2Simplex(vector<v2> &simplex, v2 &searchDir) {
+	bool improve_2_simplex(vector<v2> &simplex, v2 &search_dir) {
 		assert(simplex.size() == 2);
-		assert(!containsDuplicates(simplex));
+		assert(!contains_duplicates(simplex));
 		
-		if (originIsBetweenPoints(simplex[0], simplex[1])) {
+		if (origin_is_between_points(simplex[0], simplex[1])) {
 			// the simplex is correct.
 			// search on the side of the 2-simplex that contains the origin.
-			v2 simplexLine = simplex[1] - simplex[0];
-			v2 directionToOrigin = origin - simplex[0];
-			searchDir = simplexLine.normalInDirectionOrZero(directionToOrigin);
+			v2 simplex_line = simplex[1] - simplex[0];
+			v2 direction_to_origin = origin - simplex[0];
+			search_dir = simplex_line.normal_in_direction_or_0(direction_to_origin);
 		} else if (dot(simplex[1] - simplex[0], origin - simplex[0]) <= 0) {
 			// the closest part of the simplex is the point simplex[0], so update the simplex.
 			simplex = {simplex[0]};
-			searchDir = (origin - simplex[0]).normalisedOrZero();
+			search_dir = (origin - simplex[0]).normalised_or_zero();
 		} else {
 			// the closest part of the simplex is the point simplex[1], so update the simplex.
 			assert(dot(simplex[0] - simplex[1], origin - simplex[1]) <= 0);
 			simplex = {simplex[1]};
-			searchDir = (origin - simplex[1]).normalisedOrZero();
+			search_dir = (origin - simplex[1]).normalised_or_zero();
 		}
 		
-		if (searchDir.isZero()) {
+		if (search_dir.is_zero()) {
 			return true; // the origin is precisely on the simplex edge.
 		}
 		
 		assert(simplex.size() == 1 || simplex.size() == 2);
-		assert(searchDir.length() > 0.9999 && searchDir.length() < 1.0001);
+		assert(search_dir.length() > 0.9999 && search_dir.length() < 1.0001);
 		return false;
 	}
 	
 	// returns true when the simplex contains the origin.
-	bool improveSimplex(vector<v2> &simplex, v2 &searchDir) {
+	bool improve_simplex(vector<v2> &simplex, v2 &search_dir) {
 		if (simplex.size() == 3) {
-			v2 abNormalAwayFromC = (simplex[1] - simplex[0]).normalInDirectionOrZero(simplex[0] - simplex[2]);
-			assert(!abNormalAwayFromC.isZero());
-			v2 bcNormalAwayFromA = (simplex[2] - simplex[1]).normalInDirectionOrZero(simplex[1] - simplex[0]);
-			assert(!bcNormalAwayFromA.isZero());
-			v2 caNormalAwayFromB = (simplex[0] - simplex[2]).normalInDirectionOrZero(simplex[2] - simplex[1]);
-			assert(!caNormalAwayFromB.isZero());
+			v2 ab = simplex[1] - simplex[0];
+			v2 bc = simplex[2] - simplex[1];
+			v2 ca = simplex[0] - simplex[2];
+			v2 ab_normal_away_from_c = ab.normal_in_direction_or_0(ca);
+			v2 bc_normal_away_from_a = bc.normal_in_direction_or_0(ab);
+			v2 ca_normal_away_from_b = ca.normal_in_direction_or_0(bc);
 			
 			// find which side of the triangle the origin is on, or if it's inside it.
-			if (dot(abNormalAwayFromC, origin - simplex[0]) > 0) {
+			if (dot(ab_normal_away_from_c, origin - simplex[0]) > 0) {
 				simplex = {simplex[0], simplex[1]};
-				return improve2Simplex(simplex, searchDir);
-			} else if (dot(bcNormalAwayFromA, origin - simplex[1]) > 0) {
+				return improve_2_simplex(simplex, search_dir);
+			} else if (dot(bc_normal_away_from_a, origin - simplex[1]) > 0) {
 				simplex = {simplex[1], simplex[2]};
-				return improve2Simplex(simplex, searchDir);
-			} else if (dot(caNormalAwayFromB, origin - simplex[2]) > 0) {
+				return improve_2_simplex(simplex, search_dir);
+			} else if (dot(ca_normal_away_from_b, origin - simplex[2]) > 0) {
 				simplex = {simplex[2], simplex[0]};
-				return improve2Simplex(simplex, searchDir);
+				return improve_2_simplex(simplex, search_dir);
 			} else {
 				return true; // the origin is inside the simplex.
 			}
 		}
 		
 		assert(simplex.size() == 2);
-		return improve2Simplex(simplex, searchDir);
+		return improve_2_simplex(simplex, search_dir);
 	}
 	
-	bool shapesAreClose(Shape *shapeA, Shape *shapeB) {
-		return shapeA->pos.x+shapeA->radius >= shapeB->pos.x-shapeB->radius
-			&& shapeA->pos.x-shapeA->radius <= shapeB->pos.x+shapeB->radius
-			&& shapeA->pos.y+shapeA->radius >= shapeB->pos.y-shapeB->radius
-			&& shapeA->pos.y-shapeA->radius <= shapeB->pos.y+shapeB->radius;
+	bool shapes_are_close(Shape *a, Shape *b) {
+		return a->pos.x+a->radius >= b->pos.x-b->radius
+			&& a->pos.x-a->radius <= b->pos.x+b->radius
+			&& a->pos.y+a->radius >= b->pos.y-b->radius
+			&& a->pos.y-a->radius <= b->pos.y+b->radius;
 	}
 	
-	bool shapesAreOverlapping(
+	bool shapes_are_overlapping(
 		Shape *shapeA, Shape *shapeB,
 		vector<v2> *simplexOut = nullptr // This is only used internally.
 		) {
 		
-		if (!shapesAreClose(shapeA, shapeB)) return false;
+		if (!shapes_are_close(shapeA, shapeB)) return false;
 		
 		{ // TODO: unfinished code for correctly setting TINY_NUMBER.
 			double largestRadius = max(shapeA->radius, shapeB->radius);
 			
 		}
 		
-		v2 searchDirection = v2(0, 1); // this can be anything except zero
-		vector<v2> simplex = {getMinkowskiDiffedEdge(shapeA, shapeB, searchDirection)};
+		v2 search_direction = v2(0, 1); // this can be anything except zero
+		vector<v2> simplex = {get_minkowski_diffed_edge(shapeA, shapeB, search_direction)};
 		
-		searchDirection = origin - simplex[0]; // search toward the origin
+		search_direction = origin - simplex[0]; // search toward the origin
 		
 		while (true) {
 			assert(simplex.size() < 3);
-			simplex.push_back(getMinkowskiDiffedEdge(shapeA, shapeB, searchDirection));
+			simplex.push_back(get_minkowski_diffed_edge(shapeA, shapeB, search_direction));
 			
-			double dotResult = dot(simplex.back(), searchDirection);
+			double dotResult = dot(simplex.back(), search_direction);
 			if (dotResult <= TINY_NUMBER) {
 				return false;
 			}
 			
-			assert(!containsDuplicates(simplex));
+			assert(!contains_duplicates(simplex));
 			
-			if (improveSimplex(simplex, searchDirection)) {
+			if (improve_simplex(simplex, search_direction)) {
 				if (simplexOut != nullptr) *simplexOut = simplex;
 				return true;	
 			}
 		}
 	}
 	
-	OverlapInfo getOverlapInfo(Shape *shapeA, Shape *shapeB) {
+	Overlap_Info get_overlap_info(Shape *shapeA, Shape *shapeB) {
 		
-		auto getAmountForOriginOnEdge = [shapeA, shapeB]() {
+		auto get_amount_for_origin_on_edge = [shapeA, shapeB]() {
 			v2 posDifference = shapeB->pos - shapeA->pos;
 			
-			if (posDifference.isZero()) {
+			if (posDifference.is_zero()) {
 				posDifference.x = TINY_NUMBER;
 			}
 			
-			return posDifference.normalisedOrZero() * TINY_NUMBER;
+			return posDifference.normalised_or_zero() * TINY_NUMBER;
 		};
 		
-		OverlapInfo info;
+		Overlap_Info info;
 		vector<v2> simplex;
 		
-		info.overlapping = shapesAreOverlapping(shapeA, shapeB, &simplex);
+		info.overlapping = shapes_are_overlapping(shapeA, shapeB, &simplex);
 		
 		if (info.overlapping) {
 			if (simplex.size() < 2) {
-				info.amount = getAmountForOriginOnEdge();
+				info.amount = get_amount_for_origin_on_edge();
 				return info;
 			}
 			
 			while (true) {
 				// find simplex line closest to origin
-				int closestEdgeIndex = 0;
+				int closest_edge_index = 0;
 				{
-					double closestDistance = INFINITY;
+					double closest_distance = INFINITY;
 					
 					for (int s0 = 0; s0 < simplex.size(); s0++) {
 						int s1 = (s0 + 1) % simplex.size();
 						int s2 = (s0 + 2) % simplex.size();
 						
-						v2 simplexLine = simplex[s1] - simplex[s0];
-						v2 approxOuterNormalDir = simplex[s0] - simplex[s2];
-						v2 outerNormal = simplexLine.normalInDirectionOrZero(approxOuterNormalDir);
+						v2 simplex_line = simplex[s1] - simplex[s0];
+						v2 approx_outer_normal_dir = simplex[s0] - simplex[s2];
+						v2 outer_normal = simplex_line.normal_in_direction_or_0(approx_outer_normal_dir);
 						
-						if (outerNormal.isZero()) {
-							info.amount = getAmountForOriginOnEdge();
+						if (outer_normal.is_zero()) {
+							info.amount = get_amount_for_origin_on_edge();
 							return info;
 						}
 						
-						double distance = dot(outerNormal, simplex[s0]);
-						if (distance < closestDistance) {
-							closestDistance = distance;
-							closestEdgeIndex = s0;
+						double distance = dot(outer_normal, simplex[s0]);
+						if (distance < closest_distance) {
+							closest_distance = distance;
+							closest_edge_index = s0;
 						}
 					}
 				}
 				
 				// get outer normal of the line and get minkowski diffed edge in that direction
-				v2 newSimplexCrnr;
-				int s0 = closestEdgeIndex;
-				int s1 = (closestEdgeIndex + 1) % simplex.size();
-				int s2 = (closestEdgeIndex + 2) % simplex.size();
+				v2 new_simplex_corner;
+				int s0 = closest_edge_index;
+				int s1 = (closest_edge_index + 1) % simplex.size();
+				int s2 = (closest_edge_index + 2) % simplex.size();
 				{
-					v2 outerNormal = (simplex[s1] - simplex[s0]).normalInDirectionOrZero(simplex[s0] - simplex[s2]);
-					assert(!outerNormal.isZero());
-					newSimplexCrnr = getMinkowskiDiffedEdge(shapeA, shapeB, outerNormal);
+					v2 outer_normal = (simplex[s1] - simplex[s0]).normal_in_direction_or_0(simplex[s0] - simplex[s2]);
+					assert(!outer_normal.is_zero());
+					new_simplex_corner = get_minkowski_diffed_edge(shapeA, shapeB, outer_normal);
 				}
 				
 				// if the new edge is almost identical to one of the points that made the simplex line,
-				if (newSimplexCrnr.distance(simplex[s0]) < TINY_NUMBER || newSimplexCrnr.distance(simplex[s1]) < TINY_NUMBER) {
+				if (new_simplex_corner.distance(simplex[s0]) < TINY_NUMBER || new_simplex_corner.distance(simplex[s1]) < TINY_NUMBER) {
 					// find the point on the line that is closest to the origin
-					v2 outerNormal = (simplex[s1] - simplex[s0]).normalInDirectionOrZero(simplex[s0] - simplex[s2]);
-					assert(!outerNormal.isZero());
-					double lengthOfPoint = dot(outerNormal, simplex[s0]);
+					v2 outer_normal = (simplex[s1] - simplex[s0]).normal_in_direction_or_0(simplex[s0] - simplex[s2]);
+					assert(!outer_normal.is_zero());
+					double length_of_point = dot(outer_normal, simplex[s0]);
 					
 					// the difference between the origin and that point is the overlap amount.
-					info.amount = outerNormal.normalisedOrZero() * (lengthOfPoint + TINY_NUMBER);
+					info.amount = outer_normal.normalised_or_zero() * (length_of_point + TINY_NUMBER);
 				
 					// break out of the loop
 					break;
 				} else {
 					// add the new corner to the simplex, turning the existing line into two.
-					simplex.insert(simplex.begin()+s1, newSimplexCrnr);
-					assert(!containsDuplicates(simplex));
+					simplex.insert(simplex.begin()+s1, new_simplex_corner);
+					assert(!contains_duplicates(simplex));
 				}
 			}
 		}
--- tests.cpp
+++ tests.cpp
@@ -16,81 +16,104 @@ to clear the terminal beforehand and delete the executable after I'm done with i
 
 using namespace rw_gjk;
 
-void printTestName(string name) {
+void print_test_name(string name) {
 	while (name.size() < 60) {
 		name.push_back('_');
 	}
 	printf("%s", name.c_str());
 }
 
-void printTestResult(bool success) {
+void print_test_result(bool success) {
 	printf(success ? "success\n" : "FAIL\n");
 }
 
 int main() {
-	printf("\n * Running tests for rw_gjk *\n\n");
+	printf("\n * Running tests for rw_gjk *\n");
 	
 	Shape shape;
 	
-	printf("tryMakeShape():\n");
+	printf("\ntry_make_shape():\n");
 	{
-		printTestName("Valid shape with clockwise winding");
+		print_test_name("Valid shape with clockwise winding");
 		vector<v2> corners = { v2(0, 0), v2(0, 1), v2(1, 1) };
-		printTestResult(tryMakeShape(corners, &shape));
+		print_test_result(try_make_shape(corners, &shape));
 	}
 	
 	{
-		printTestName("Valid shape with anti-clockwise winding");
+		print_test_name("Valid shape with anti-clockwise winding");
 		vector<v2> corners = { v2(0, 0), v2(1, 0), v2(1, 1) };
-		printTestResult(tryMakeShape(corners, &shape));
+		print_test_result(try_make_shape(corners, &shape));
 	}
 	
 	{
-		printTestName("Null pointer argument");
+		print_test_name("Null pointer argument");
 		vector<v2> corners = { v2(0, 0), v2(0, 1), v2(1, 1), v2(0.1, 0.9) };
-		printTestResult(!tryMakeShape(corners, nullptr));
+		print_test_result(!try_make_shape(corners, nullptr));
 	}
 	
 	{
-		printTestName("Invalid concave shape");
+		print_test_name("Invalid concave shape");
 		vector<v2> corners = { v2(0, 0), v2(0, 1), v2(1, 1), v2(0.1, 0.9) };
-		printTestResult(!tryMakeShape(corners, &shape));
+		print_test_result(!try_make_shape(corners, &shape));
 	}
 	
 	{
-		printTestName("Invalid shape, only two corners");
+		print_test_name("Invalid shape, only two corners");
 		vector<v2> corners = { v2(0, 0), v2(0, 1) };
-		printTestResult(!tryMakeShape(corners, &shape));
+		print_test_result(!try_make_shape(corners, &shape));
 	}
 	
 	{
-		printTestName("Invalid shape, only one corner");
+		print_test_name("Invalid shape, only one corner");
 		vector<v2> corners = { v2(0, 1) };
-		printTestResult(!tryMakeShape(corners, &shape));
+		print_test_result(!try_make_shape(corners, &shape));
 	}
 	
 	{
-		printTestName("Invalid shape, no corners");
+		print_test_name("Invalid shape, no corners");
 		vector<v2> corners = {};
-		printTestResult(!tryMakeShape(corners, &shape));
+		print_test_result(!try_make_shape(corners, &shape));
 	}
 	
 	{
-		printTestName("Invalid shape, corners form a straight line");
+		print_test_name("Invalid shape, corners form a straight line");
 		vector<v2> corners = {
 			v2(0, 0), v2(1, 0), v2(2, 0), v2(1, 1)
 		};
-		printTestResult(!tryMakeShape(corners, &shape));
+		print_test_result(!try_make_shape(corners, &shape));
 	}
 	
 	{
-		printTestName("Invalid shape, duplicate corners");
+		print_test_name("Invalid shape, duplicate corners");
 		vector<v2> corners = {
 			v2(0, 0), v2(0, 0), v2(1, 0), v2(0, 1)
 		};
-		printTestResult(!tryMakeShape(corners, &shape));
+		print_test_result(!try_make_shape(corners, &shape));
+	}
+	
+	printf("\nget_overlap_info():\n");
+	Shape shape_a, shape_b;
+	try_make_shape({v2(-1, -0.5), v2(1, -0.5), v2(0, 1)}, &shape_a);
+	try_make_shape({v2(-1, -0.5), v2(1, -0.5), v2(0, 1)}, &shape_b);
+	{
+		print_test_name("Shapes overlap");
+		shape_a.pos = v2(42, 42);
+		shape_b.pos = v2(42, 42);
+		print_test_result(get_overlap_info(&shape_a, &shape_b).overlapping);
+	}
+	
+	{
+		print_test_name("Shapes don't overlap");
+		shape_a.pos = v2(-10, 3);
+		shape_b.pos = v2(10, 3);
+		print_test_result(!get_overlap_info(&shape_a, &shape_b).overlapping);
 	}
 	
 	printf("\n");
 	return 0;
 }
+
+
+
+
+
